7a42502da3a17276eee7bac4fa2976fa44cb7461
Orphee241 <nzienguiakoumbou@gmail.com>
2022-05-28 11:13:23 +0100

    Vesion finale

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ let block = document.getElementById("blocksection2");
 let next = document.querySelector(".suivant")
 let previous = document.querySelector(".retour")
 
+let next2 = document.querySelector(".suivant")
+
 next.onclick = function(){
     block.style.transform = "translateX(-183px)";
     previous.style.display = "flex"
 }
+
+next2.onclick = function(){
+    block.style.transform = "translateX(-183px)";
+    previous.style.display = "flex"
+}
+
 previous.onclick = function(){
     block.style.transform = "translateX(0px)";
     previous.style.display = "none"
